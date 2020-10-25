e8861b14f46650e6b606c0ea8b75adc7f9397359
jerry <root@localost>
2020-10-25 10:57:14 +0200

    ESP and other

---
+++ ESP_API/temp_outside/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace temp_outside
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.показатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.скрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.автозапускToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.обновитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label1.Name = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            resources.ApplyResources(this.notifyIcon1, "notifyIcon1");
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.обновитьToolStripMenuItem,
+            this.показатьToolStripMenuItem,
+            this.скрытьToolStripMenuItem,
+            this.выходToolStripMenuItem,
+            this.автозапускToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            resources.ApplyResources(this.contextMenuStrip1, "contextMenuStrip1");
+            // 
+            // показатьToolStripMenuItem
+            // 
+            this.показатьToolStripMenuItem.Name = "показатьToolStripMenuItem";
+            resources.ApplyResources(this.показатьToolStripMenuItem, "показатьToolStripMenuItem");
+            this.показатьToolStripMenuItem.Click += new System.EventHandler(this.показатьToolStripMenuItem_Click);
+            // 
+            // скрытьToolStripMenuItem
+            // 
+            this.скрытьToolStripMenuItem.Name = "скрытьToolStripMenuItem";
+            resources.ApplyResources(this.скрытьToolStripMenuItem, "скрытьToolStripMenuItem");
+            this.скрытьToolStripMenuItem.Click += new System.EventHandler(this.скрытьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            resources.ApplyResources(this.выходToolStripMenuItem, "выходToolStripMenuItem");
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // автозапускToolStripMenuItem
+            // 
+            this.автозапускToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьToolStripMenuItem,
+            this.удалитьToolStripMenuItem});
+            this.автозапускToolStripMenuItem.Name = "автозапускToolStripMenuItem";
+            resources.ApplyResources(this.автозапускToolStripMenuItem, "автозапускToolStripMenuItem");
+            // 
+            // добавитьToolStripMenuItem
+            // 
+            this.добавитьToolStripMenuItem.Name = "добавитьToolStripMenuItem";
+            resources.ApplyResources(this.добавитьToolStripMenuItem, "добавитьToolStripMenuItem");
+            this.добавитьToolStripMenuItem.Click += new System.EventHandler(this.добавитьToolStripMenuItem_Click);
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            resources.ApplyResources(this.удалитьToolStripMenuItem, "удалитьToolStripMenuItem");
+            this.удалитьToolStripMenuItem.Click += new System.EventHandler(this.удалитьToolStripMenuItem_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // обновитьToolStripMenuItem
+            // 
+            this.обновитьToolStripMenuItem.Name = "обновитьToolStripMenuItem";
+            resources.ApplyResources(this.обновитьToolStripMenuItem, "обновитьToolStripMenuItem");
+            this.обновитьToolStripMenuItem.Click += new System.EventHandler(this.обновитьToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem показатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem скрытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem автозапускToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьToolStripMenuItem;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripMenuItem обновитьToolStripMenuItem;
+    }
+}
+
---
+++ ESP_API/temp_outside/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Json;
+using System.Net.WebSockets;
+using System.Net;
+using System.Configuration;
+using System.Web;
+using IniParser;
+using IniParser.Model;
+using System.Text.RegularExpressions;
+using System.Resources;
+using temp_outside.Properties;
+using static temp_outside.Properties.Resources;
+
+
+namespace temp_outside
+{
+    public partial class Form1 : Form
+    {
+        static string AppNameFile = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+        private static string settingPath = AppDomain.CurrentDomain.BaseDirectory + AppNameFile + ".ini";
+        public const string DEFAULT_SECTION = "app";
+        public const string DEFAULT_FILENAME = "Config.ini";
+
+        public static void CreateINIData()//create INI data file data
+        {
+            var data = new IniData();
+            IniData createData = new IniData();
+            FileIniDataParser iniParser = new FileIniDataParser();
+
+            createData.Sections.AddSection(DEFAULT_SECTION);
+            createData.Sections.GetSectionData(DEFAULT_SECTION).Comments.Add("This is the configuration file for the" + AppNameFile);
+            createData.Sections.GetSectionData(DEFAULT_SECTION).Keys.AddKey("name", "Температура на улице");
+            createData.Sections.GetSectionData(DEFAULT_SECTION).Keys.AddKey("url", "http://senseair.local/sensor/senseair_temperatura_out");
+            createData.Sections.GetSectionData(DEFAULT_SECTION).Keys.AddKey("value", "test");
+            iniParser.WriteFile(settingPath, createData);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            
+            if (!File.Exists(settingPath))
+            {
+                DialogResult result = MessageBox.Show(
+            "Файла нет конфигурации. Создаю",
+            "Сообщение",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Information,
+            MessageBoxDefaultButton.Button1,
+            MessageBoxOptions.DefaultDesktopOnly);
+
+            if (result == DialogResult.OK)
+               { 
+               CreateINIData();
+               }
+            }
+            //else
+            //{
+            //    MessageBox.Show("Файл!");
+            //}
+
+
+
+            var parser = new FileIniDataParser();
+            
+            IniData data = parser.ReadFile(AppNameFile + ".ini");
+            string appname = data["app"]["name"];
+            string appuri = data["app"]["url"];
+            string appvalue = data["app"]["value"];
+            this.Text = appname;
+            notifyIcon1.Visible = true;
+            notifyIcon1.BalloonTipTitle = appname ;
+            notifyIcon1.BalloonTipText = "Читаю";
+            notifyIcon1.ShowBalloonTip(10000);
+            this.ShowInTaskbar = false;
+            this.WindowState = FormWindowState.Minimized;
+            timer2.Enabled = true;
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // string ConnectionString = "http://senseair.local/sensor/senseair_temperatura_out";
+
+            var parser = new FileIniDataParser();
+            string AppNameFile = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+            IniData dataini = parser.ReadFile(AppNameFile + ".ini");
+            string appuri = dataini["app"]["url"];
+            string appvalue = dataini["app"]["value"];
+
+            string ConnectionString = appuri;
+
+            using (WebClient httpClient = new WebClient())
+            {
+                httpClient.Encoding = Encoding.UTF8;
+
+                List<string> data = new List<string>();
+                var jsonData = httpClient.DownloadString(ConnectionString);
+
+                dynamic result = JsonValue.Parse(jsonData);
+
+
+                if (result.ContainsKey("state"))
+                {
+                    dynamic value = result["state"];
+                    label1.Text = value.ToString();
+                    notifyIcon1.Text = label1.Text;
+                }
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button1_Click(null, null);
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.CenterToScreen();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void показатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void скрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void добавитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            key.SetValue("ESPHOME API", Application.ExecutablePath);
+        }
+
+        private void удалитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            key.DeleteValue("ESPHOME API", false);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            button1_Click(null, null);
+            timer2.Enabled = false;
+        }
+
+        private void обновитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button1_Click(null, null);
+        }
+    }
+}
