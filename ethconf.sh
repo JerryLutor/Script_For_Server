a4af9cb04e83667496d2a5c732565a6caf9b56e4
jerry <root@localost>
2021-04-18 16:02:38 +0300

    upd

--- ethconf.sh
+++ ethconf.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 tool="/sbin/ethtool"
+${tool} -C $1 adaptive-rx off adaptive-tx off
 ${tool} -G $1 tx 4096 rx 4096
 ${tool} -C $1 tx-frames-irq 2048
 ${tool} -C $1 rx-usecs 30
 ${tool} -A $1 tx off rx off
+
 exit 0
