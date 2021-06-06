68ac0a99a12d77628fce930d6e62c32bb5ad7874
jerry <root@localost>
2021-06-06 19:36:14 +0300

    Update upd_geodatebase_base.sh

--- upd_geodatebase_base.sh
+++ upd_geodatebase_base.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 
+#rm /opt/flussonic/lib/egeoip2/priv/GeoLite2-Country.mmdb;
 curl -s https://api.github.com/repos/P3TERX/GeoLite.mmdb/releases/latest \
 | grep "browser_download_url.*GeoLite2-Country.mmdb" \
 | cut -d '"' -f 4 \
-| wget -qi - -O /opt/flussonic/lib/egeoip2/priv/GeoLite2-Country.mmdb
+| wget -qi - -m -c -np -O /opt/flussonic/lib/egeoip2/priv/GeoLite2-Country.mmdb;
+
+ls -n /opt/flussonic/lib/egeoip2/priv;
+
+
